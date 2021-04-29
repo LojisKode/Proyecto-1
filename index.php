991e12bd0958717b631870690b766cbdc823ff8e
Victor Salazar <lojiskode@gmail.com>
2021-04-29 20:27:03 -0300

    cambios

--- index.php
+++ index.php
@@ -2,44 +2,65 @@
 <html>
 
 <head>
-    <title>Login</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Index</title>        
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <!-- Font Awesome -->
+        <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"/>
+        <!-- Style.css -->
+        <link rel="stylesheet" href="styleNavbar.css">
+        <!-- Scripts -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+        <!-- Custom Script -->
+        <script src="jsNavbar.js"></script>
 </head>
 
 <body>
+    <!--Navbar-->
+    <nav class="navbar navbar-expand-lg navbar-mainbg">
+            <a class="navbar-brand navbar-logo" href="index.php">Bienvenido </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars text-white"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <div class="hori-selector">
+                        <div class="left"></div>
+                        <div class="right"></div>
+                    </div> 
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#"><i class="fas fa-spa"></i> Tratamientos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Doctores</a>
+                    </li> 
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-book"></i> Consultas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-user-graduate"></i> Contacto</a>
+                    </li> 
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt"></i> Login</a>
+                    </li>
+                </ul>                    
+            </div>                
+        </nav>     
     <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar w/ text</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
-                        </li>
-                    </ul>
-                    <a href="login.php">                    
-                    <span class="navbar-item">
-                        <span span class="icone icone-custom pull-right"><i class="fa fa-user-circle-o" aria-hidden="true">Ingresar</i></span>
-                    </span>
-                    </a>
-                </div>
-            </div>
-        </nav>
-    </div>
-    <div>
-        <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+    <a class="ir-arriba fixed-bottom"  javascript:void(0) title="Volver arriba">
+  <span class="fa-stack">
+    <i class="fa fa-circle fa-stack-2x"></i>
+    <i class="fa fa-arrow-up fa-stack-1x fa-inverse"></i>
+  </span>
+</a>
+        <!--Slider-->
+        <div id="carousel" class="carousel slide p-100 mt-150" data-bs-ride="carousel">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -85,7 +106,7 @@
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
     </div>
-
+    
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -8,24 +8,70 @@
     <meta charset="UTF-8">
 </head>
 
-<body>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-sm">
-                <form action="admin.php" method="post">
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Usuario</label>
-                        <input type="email" class="form-control" name="user" placeholder="Usuario">
+<body><!DOCTYPE html>
+<html>
+
+<head>    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="styleLogin.css">
+
+</head>
+    <body>
+    <div class="container w-75 bg-primary mt-4 rounded shadow">
+        <div class="row align-item-stretch">
+            <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 rounded">            
+            </div>
+            <div class="col bg-white p-5 rounded-end">
+                <div class="text-end">
+                    <img src="img/logo.jpg" width="48" alt="">
+                </div>
+                <h2 class="fw-bold text-center py-5">Bienvenido</h2>
+
+                <!--login-->
+                <form action="index.php" method="post">
+                    <div class="mb-4">
+                        <label for="email" class="form-label">Usuario</label>
+                        <input type="text" class="form-control" name="user">
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                        <input type="password" class="form-control" name="password" placeholder="Contraseña">
+                    <div class="mb-4">
+                        <label for="email" class="form-label">Contraseña</label>
+                        <input type="password" class="form-control" name="password">
                     </div>
-                    <button type="submit" class="btn btn-primary" name="entrar">Iniciar Sesion</button>
-                </form>
+                    <div class="mb-4 form-check">
+                        <input type="checkbox" name="connected" class="form-check-input" id="">
+                        <label for="connected" class="form-check-label"> Mantenerme conectado</label>
+                    </div>
+
+                    <div class="container" w-100 my-5>
+                    <div class="row">
+                        <div class="col">
+                            <button class="btn btn-outline-success w-100 my-1" action="#" method="post" name="login">
+                                <div class="row align-item-center">
+                                    <div class="col-2 d-none d-md-block">                                        
+                                    </div>
+                                    <div class="text-center">Iniciar Sesion</div>
+                                </div>
+                            </button>
+                        </div>
+                        <div class="col">
+                            <button class="btn btn-outline-danger w-100 my-1" action="index.php">
+                                <div class="row align-item-center">
+                                    <div class="col-2 d-none d-md-block">
+                                    </div>
+                                    <div class="text-center">Volver</div>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div>                            
             </div>
         </div>
     </div>
-</body>
-
+    </body>
+</html>
 </html>
